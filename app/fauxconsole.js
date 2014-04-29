4ef7ba97e70eb65f25de69319e9d824fda0de467
pangguoqing <pangguoqing1984@126.com>
2014-04-29 20:14:40 +0800

    [add] module_define

---
+++ app/fauxconsole.js
@@ -0,0 +1,51 @@
+﻿/* Faux Console by Chris Heilmann http://wait-till-i.com */if (!window.console) {
+	var console = {
+		init : function() {
+			console.d = document.createElement('div');
+			document.body.appendChild(console.d);
+			var a = document.createElement('a');
+			a.href = 'javascript:console.hide()';
+			a.innerHTML = 'close';
+			console.d.appendChild(a);
+			var a = document.createElement('a');
+			a.href = 'javascript:console.clear();';
+			a.innerHTML = 'clear';
+			console.d.appendChild(a);
+			var id = 'fauxconsole';
+			if (!document.getElementById(id)) {
+				console.d.id = id;
+			}
+			console.hide();
+		},
+		hide : function() {
+			if(console.d)console.d.style.display = 'none';
+		},
+		show : function() {
+			if(console.d)console.d.style.display = 'block';
+		},
+		log : function(o) {
+			if(console.d)console.d.innerHTML += '<br/>' + o;
+			console.show();
+		},
+		clear : function() {
+			console.d.parentNode.removeChild(console.d);
+			console.init();
+			console.show();
+		},/* Simon Willison rules */
+		addLoadEvent : function(func) {
+			var oldonload = window.onload;
+			if (typeof window.onload != 'function') {
+				window.onload = func;
+			} else {
+				window.onload = function() {
+					if (oldonload) {
+						oldonload();
+					}
+					func();
+				}
+			}
+			;
+		}
+	};
+	console.addLoadEvent(console.init);
+}
